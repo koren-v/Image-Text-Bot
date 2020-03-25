16a03465f2b3cb153c7295f27a6c167e2d017e64
Vitaliy <1999kvo@gmail.com>
2020-03-25 08:05:04 +0200

    Revert "fixed"
    This reverts commit 406338860b059127666afcc75af6e07a7076e8b1.

--- data_loader.py
+++ data_loader.py
@@ -173,7 +173,7 @@ class CoCoDataset(data.Dataset):
         self.sel_length = np.random.choice(self.caption_lengths)
         # select their indexes
         # all_indices = np.where([self.caption_lengths[i] == sel_length for i in np.arange(len(self.caption_lengths))])[0]
-        all_indices = np.where([self.caption_lengths[i] >= self.sel_length-2 or self.caption_lengths[i] <= self.sel_length+2 for i in np.arange(len(self.caption_lengths))])[0]
+        all_indices = np.where([self.caption_lengths[i] >= sel_length-2 or self.caption_lengths[i] <= sel_length+2 for i in np.arange(len(self.caption_lengths))])[0]
         # select batch_size indexes of the chosen lenght
         indices = list(np.random.choice(all_indices, size=self.batch_size))
         return indices #, sel_length + 1
